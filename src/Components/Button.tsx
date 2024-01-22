63e9b3b513bd8fc9223df93fc7ed6dddeb945a4e
Zaid <syedzaidahmed21@gmail.com>
2024-01-22 17:10:45 +0500

    mapeed the buttons

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,7 +1,10 @@
-const Button = () => {
+interface val {
+  value: number;
+}
+const Button: React.FC<val> = ({ value }) => {
   return (
     <div className="bg-[#292929] text-white h-[3rem] w-[3rem] rounded-full flex justify-center items-center">
-      <h6 className="font-semibold text-2xl">1</h6>
+      <h6 className="font-semibold text-2xl">{value}</h6>
     </div>
   );
 };
--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import Button from "./Button";
 
 const Calculator = () => {
+  const [values, setValues] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
   return (
     <div className="calculator main bg-black h-[65vh] w-[20vw] rounded-2xl">
-      <Button />
+      {values.map((value, idx) => {
+        return <Button value={value} key={idx} />;
+      })}
     </div>
   );
 };

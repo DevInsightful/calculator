3e1e0e9ac189948e44f10a619727726fd062a106
Zaid <syedzaidahmed21@gmail.com>
2024-01-22 11:34:27 +0500

    main calculator screen added

---
+++ src/Components/Calculator.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Calculator = () => {
+  return (
+    <div className="calculator main bg-black h-[65vh] w-[20vw] rounded-2xl"></div>
+  );
+};
+
+export default Calculator;

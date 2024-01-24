8f561f874a8aa204feaa07df5f4629d8a18899cf
Zaid <syedzaidahmed21@gmail.com>
2024-01-24 20:08:17 +0500

    unwanted removed code optizmized

--- src/Components/Screen.tsx
+++ src/Components/Screen.tsx
@@ -1,10 +1,7 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
 
 const Screen: React.FC<{ query: any }> = ({ query }) => {
   const [screenContent, setScreenContent] = useState<string | null>(null);
-
-  // Use useMemo to track the previous value
-  const previousQuery = useMemo(() => screenContent, [screenContent]);
 
   useEffect(() => {
     // Update screenContent based on the previous value and the new query
